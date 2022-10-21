8161c93d203a327548b1233b9c13c248cd0e2832
berkcangumusisik <gumusisikberkcan@gmail.com>
2022-10-21 09:44:23 +0300

    Java 1. Gün çalışmaları eklendi.

---
+++ 01.HelloWorld/src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Java Öğrenmeye Başlıyoruz !");
+        // Tek satır yorum satırı
+        /* Çoklu
+        yorum
+        satırı*/
+    }
+}
+/**
+ Java Tarihçesi
+ * 1991 yılında Sun Microsystems şirketi mühendislerinden James Gosling ve 12 arkadaşı Green Project(Yeşil Proje) isimli bir proje geliştirmeye başladılar.
+ * Proje C ve C++ kullansalarda yetersiz kaldığını düşünmüşler.
+ * Oak adı verilen Java'nın ilk adıdır.
+ * Java C ve C++ ile bağlantılıdır.
+ * C'nin synatx'ını ve C++'un object oriented yapısını birleştirmiştir.
+ Java'nın Avantajları
+ * Basit bir dil olması
+ * Tarafsız mimariye sahip olması(Platform bağımsızdır)
+ * Nesne yönelimli olması
+ * Zengin kütüphane desteği
+ * Multithreaded olması(çoklu iş parçacığı)
+ * Dinamik olması
+ * En çok tercih edilen dillerden olması
+ Java ile Geliştirme Yapılabilen Alanlar
+ - Mobil Uygulamalar
+ - Web Uygulamaları
+ - Masaüstü Uygulamaları
+ - Oyun Geliştirme
+ - Bilimsel Uygulamalar
+ - Gömülü Sistemler
+ - Kurumsal Uygulamalar
+ Ekrana Veri Yazdırma
+ * System.out.println(); Ekrana veri yazdırır ve alt satıra geçer.
+ Yorum Satırları
+ * Kod içi belgeleme amacıyla kullanılan ve kodun çalıştığında etkisizleşen satırlardır.
+ * Yazılımcılar tarafından yazılan kodun anlaşılmasını kolaylaştırmak için kullanılır.
+ * // Tek satırlık yorum satırı
+ ****Java da satır sonuna ; konulur.
+ */
+/*Çok satırlık yorum satırı */
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# javaOgrenmeSerisi
+# Java Öğrenme Serisi
+
+Bu reponun amacı kendimi Java alanında geliştirirken Java'ya yeni başlayacak arkadaşlara yol gösterici olmak istedim. Bu repoda gün gün ne çalıştığım, notlarım, yazdığım kodlar ve yaptığım projeler yer alacaktır. Tek bir kaynaktan gitmeyerek bir çok kaynak tarayarak bu notları oluşturuyorum. Eğer hoşunuza giderse ⭐vererek destek olmayı unutmazsanız çok sevinirim.
+
+![](https://www.vectorlogo.zone/logos/java/java-ar21.png)
+
+### 1.Gün
+
+- Java Temelleri
+- Java Tarihçesi
+- Java'nın Avantajları
+- Java İle Geliştirme Yapılabilecek Ortamlar
+- Ekrana Veri Yazdırma
+- Yorum Satırları
