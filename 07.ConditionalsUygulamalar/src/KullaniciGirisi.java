84b748fbb4f2842bdb5872dbc74f83f80db4a256
berkcangumusisik <gumusisikberkcan@gmail.com>
2022-11-07 19:50:08 +0300

    Updated.

--- 07.ConditionalsUygulamalar/src/KullaniciGirisi.java
+++ 07.ConditionalsUygulamalar/src/KullaniciGirisi.java
@@ -16,3 +16,8 @@ public class KullaniciGirisi {
         }
     }
 }
+
+/**
+ * Ödev
+Eğer şifre yanlış ise kullanıcıya şifresini sıfırlayıp sıfırlamayacağını sorun, eğer kullanıcı sıfırlamak isterse yeni girdiği şifrenin hatalı girdiği ve unuttuğu şifre ile aynı olmaması gerektiğini kontrol edip , şifreler aynı ise ekrana "Şifre oluşturulamadı, lütfen başka şifre giriniz." sorun yoksa "Şifre oluşturuldu" yazan programı yazınız.
+ */
